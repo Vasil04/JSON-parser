09ea53ef3c7924f0540c6427cf8b55db3bb59767
Vasil04 <vdvasilev04@abv.bg>
2023-05-24 13:34:56 +0300

    Validate 2/3 ready

--- mainv2.cpp
+++ mainv2.cpp
@@ -3,11 +3,25 @@
 #include <cctype>
 #include <map>
 #include <fstream>
-#include <KVPairs.h>
+#include <KVPairs.hpp>
 
 using std::string, std::cin, std::cout, std::map, std::ifstream, std::endl;
 
+void skipWhiteSpaces(const string& jsonTXT, int& position){
+
+    while(position < jsonTXT.length() && std::isspace(jsonTXT[position])) position++;
+
+}
+
+bool validate(const string& jsonTXT) {
+    size_t position = 0;
+
+}
+
+// bool 
+
 int main(){
 
+    
     
 }
